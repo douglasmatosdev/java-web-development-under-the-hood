54b54323e5fcb16315e0aa4e7813b4bf727bccda
douglasmatosdev <douglasmatosdev@gmail.com>
2023-04-27 20:19:20 -0300

    Servlets and JSP together

--- Restaurant/src/main/java/com/virtualpairprogrammers/servlets/MenuSearchServlet.java
+++ Restaurant/src/main/java/com/virtualpairprogrammers/servlets/MenuSearchServlet.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,30 +19,16 @@ import com.virtualpairprogrammers.domain.MenuItem;
 @WebServlet("/searchResults.html")
 public class MenuSearchServlet extends HttpServlet {
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html");
-		
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
 		String searchTerm = request.getParameter("searchTerm");
-		
 		MenuDao menuDao = MenuDaoFactory.getMenuDao();
 		List<MenuItem> menuItems = menuDao.find(searchTerm);
 		
-		if (menuItems.size() > 0) {
+		request.setAttribute("menuItems", menuItems);
 		
-			out.println("<html><body><h1>Ricky's Restaurant</h1>");
-			out.println("<h2>Dishes containing " + searchTerm + "</h2><ul>");
-			for (MenuItem menuItem : menuItems) {
-				out.println("<li>" + menuItem + " " + menuItem.getDescription() + "</li>");
-			}
-			
-			out.println("</ul></body></html>");
-		}
-		else {
-			out.println("<html><body><h1>Ricky's Restaurant</h1>");
-			out.println("<p>I'm sorry, there are no dishes containing " + searchTerm);
-			out.println("</p></body></html>");
-		}
-		out.close();
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher("/searchResults.jsp");
+		dispatcher.forward(request, response);
 	}
 }
